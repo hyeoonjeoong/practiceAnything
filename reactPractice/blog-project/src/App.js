ced67701ca93f14c07fae0c8c940b843b446547b
hyeoonjeoong <jeooooong22@gmail.com>
2023-12-24 00:01:54 +0900

    ✨ 좋아요 기능 추가

--- reactPractice/blog-project/src/App.js
+++ reactPractice/blog-project/src/App.js
@@ -7,34 +7,57 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [like, setLike] = useState(0);
+
   const post = [
     {
       postId: 1,
       title: "남자 코트 추천",
       date: "1월 18일 발행",
       content: "내용내용",
+      like: like,
     },
     {
       postId: 2,
       title: "간절기 아우터 추천",
       date: "1월 28일 발행",
       content: "내용내용",
+      like: like,
     },
     {
       postId: 3,
       title: "여자 맨투맨 추천",
       date: "11월 18일 발행",
       content: "내용내용",
+      like: like,
     },
   ];
   const [postList, setPostList] = useState(post);
-  const [like, setLike] = useState(0);
   const [showModal, setShowModal] = useState(false);
 
   const handleSort = () => {
     const newPostList = [...postList];
     newPostList.sort((a, b) => a.title.localeCompare(b.title));
     console.log(newPostList);
+    setPostList(newPostList);
+  };
+
+  // const handleLike = (postId) => {
+  //   const newPostList = [...postList];
+  //   const findItem = newPostList.findIndex((post) => post.postId === postId);
+  //   const selectedList = newPostList[findItem].like;
+
+  //   setLike(selectedList.like + 1);
+
+  //   console.log(newPostList.includes(findItem));
+  //   console.log(findItem);
+  //   console.log(selectedList.like);
+  // };
+  const handleLike = (postId) => {
+    const newPostList = [...postList];
+    const findItem = newPostList.findIndex((post) => post.postId === postId);
+    const selectedPost = newPostList[findItem];
+    selectedPost.like += 1;
     setPostList(newPostList);
   };
 
@@ -50,14 +73,10 @@ function App() {
             <div className="list" key={post.postId}>
               <h4 onClick={() => setShowModal(!showModal)}>
                 {post.title}{" "}
-                <span
-                  onClick={() => {
-                    setLike(like + 1);
-                  }}
-                >
-                  👍🏻🤍{" "}
+                <span onClick={(e) => handleLike(post.postId)}>
+                  좋아요👍🏻🤍{" "}
                 </span>
-                {like}
+                {post.like}
               </h4>
               <h5>{post.date}</h5>
             </div>
