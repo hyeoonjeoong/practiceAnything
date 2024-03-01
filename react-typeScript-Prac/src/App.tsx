1bdb3a26002dedd8feed164c1c9d8353b9da0ba3
hyeoonjeoong <jeooooong22@gmail.com>
2024-03-01 23:06:34 +0900

    💡 prac

--- react-typeScript-Prac/src/App.tsx
+++ react-typeScript-Prac/src/App.tsx
@@ -5,8 +5,12 @@ import InfiniteScroll from "./components/infiniteScroll/InfiniteScroll";
 import RandomCat from "./components/intersectinoObserver/RandomCat";
 import CarouselComponent from "./components/slickCarousel/CarouselComponent";
 import MultipleCheck from "./components/multipleCheckBox/MultipleCheck";
+import RoundButton from "./components/button/RoundButton";
 
 function App() {
+  const handleButton = () => {
+    console.log("버튼 클릭");
+  };
   return (
     <>
       <h3>InfiniteScroll</h3>
@@ -17,6 +21,8 @@ function App() {
       <CarouselComponent />
       <h3>MultipleCheck</h3>
       <MultipleCheck />
+      <h3>Button type, props 처리</h3>
+      <RoundButton onClick={handleButton}>버튼</RoundButton>
     </>
   );
 }
