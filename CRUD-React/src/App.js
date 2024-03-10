3361af66cf1220f1f3cfe635aca848b0e7139f55
hyeoonjeoong <jeooooong22@gmail.com>
2024-03-10 15:53:24 +0900

    🎉 CRUD-React 폴더 생성

---
+++ CRUD-React/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoList01 from "./todo_01/TodoList01";
+
+function App() {
+  return (
+    <>
+      <h3>Todo List 01</h3>
+      <TodoList01 />
+    </>
+  );
+}
+
+export default App;
