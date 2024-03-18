ecbd421300e5a194149a8d046c2f9fcd3a94bf5c
hyeoonjeoong <jeooooong22@gmail.com>
2024-03-18 23:48:58 +0900

    💡 계획 주석 추가

--- CRUD-React/src/guestBook_01/components/SearchResult.jsx
+++ CRUD-React/src/guestBook_01/components/SearchResult.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  return (
+    <>
+      <div>SearchResult</div>
+      <ul>
+        <li>입력할 때 비밀번호, 수정 시 일치해야 하도록?</li>
+        <li>작성자, 내용 셀렉트박스로 검색되도록?</li>
+        <li>
+          이 컴포넌트 말고, 검색 컴포넌트로 변경해서, 기존 테이블에 바로
+          반영되도록?
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default SearchResult;
