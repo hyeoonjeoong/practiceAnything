6375d4e25d856c7d1def9eff2d1edd3a29968a75
hyeoonjeoong <jeooooong22@gmail.com>
2024-03-28 23:29:40 +0900

    ✨ store 등록

--- shop-project-React/src/store.js
+++ shop-project-React/src/store.js
@@ -1,6 +1,17 @@
 //redux 세팅 1.
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+//createSlice는 useState와 비슷하다고 보면 된다.
+//name : state이름
+//initialState : 초기 값
+//이렇게 만들고 store에 등록을 해야 한다.
+let user = createSlice({
+  name: 'user',
+  initialState: 'kim',
+});
 export default configureStore({
-  reducer: {},
+  reducer: {
+    // 작명 : user.reducer
+    user: user.reducer,
+  },
 });
