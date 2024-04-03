aff590d2b9bfba0c2a2109610d432b555e5c6b5b
hyeoonjeoong <jeooooong22@gmail.com>
2024-04-04 00:35:05 +0900

    ✨ paint palette

--- MemeMaker-project-js/app.js
+++ MemeMaker-project-js/app.js
@@ -1,3 +1,7 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName('color-option')
+);
+const color = document.getElementById('color');
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -30,12 +34,31 @@ function onLineWidthChange(event) {
   //console.log(event.target.value);
   ctx.lineWidth = event.target.value;
 }
+
+function onColorChange(event) {
+  //console.log(event.target.value);
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  //console.log(event.target);
+  //console.dir(event.target.dataset.color); //객체로 확인
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
 
 lineWidth.addEventListener('change', onLineWidthChange);
+color.addEventListener('change', onColorChange);
+
+//console.log(colorOptions);
+colorOptions.forEach((color) => color.addEventListener('click', onColorClick));
 
 //--------------사각형 그려보기
 // ctx.fillRect(50, 50, 100, 200); -> 얘는 단축함수
