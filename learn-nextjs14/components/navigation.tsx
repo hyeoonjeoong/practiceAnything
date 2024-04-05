325fc81234081f3eec8b810bd82c48b52a3e9955
hyeoonjeoong <jeooooong22@gmail.com>
2024-04-06 00:09:35 +0900

    ✨ use client

--- learn-nextjs14/components/navigation.tsx
+++ learn-nextjs14/components/navigation.tsx
@@ -42,12 +42,24 @@ export default function Navigation() {
 //하지만 이후에 js가 로드되는 순간, 모든 준비가 완료 된 순간 빠르게 React application이 initialize되는 것!
 //--> 예를 들어 초기에 a태그 였던 것이 리액트의 Link컴포넌트가 된다. 깜빡임 없이 더 빠르게 로드 된다.
 //그럼 리액트가 app을 넘겨받고, hard refresh 없이 navigate할 수 있게 되는 것.
+//결국 사용자가 js를 더 적게 다운받아도 된다는 의미이기에 효율적이라고 볼 수 있다.
 
 //하지만 이 Hydration이 모든 컴포넌트에서 발생하지는 않는다.
 //"use client"지시어를 맨 위에 가지고 있는 component만 ! client 에서 interactive하게 만들어진다.
 //이 컴포넌트는 client에서 interactive해야해! 라고 해주는 것.
 //초기 로드에서는 component를 render하고, HTML을 사용자에게 주고 나서, eventListener들을 추가 할 component를 hydrate한다.
 
-//그럼 언제 "use client"를 사용해야 하나?
+//✅ 그럼 언제 "use client"를 사용해야 하나?
 //남발 할 필요 없다!
-//실행했을 때 오류가 나고 next.js가 알려준다. 지금 use
+//실행했을 때 오류가 나고 next.js가 알려준다.
+// 지금 useState사용하고 있으니 (interactive 한거 사용하고 있으니) "use client" 사용해야 한다고.
+//그래서 "use client"가 client에서만 렌더링 되어야 해! 를 의미하지는 않는다.
+//--> backend에서 render되고 frontend에서 hydrate됨을 의미한다.
+//걍 use hydrate 로 생각하는게 편할 듯.
+//어쨌든 얘도 server에서 먼저 render가 되고 나서 hydrate 된다!
+
+//그럼 server component는?
+//client component를 사용하지 않는 모든 것들이 server component가 된다.
+//"use client"를 선언하지 않으면 기본적으로 모두 server component가 된다.
+//예를 들어 client에게 딱 한 번만 render되어야 한다면 (useState나 onClick Event가 없을 경우)
+//굳이 사용자에게 해당 component를 위한 코드를 다운받게 할 필요가 있나?? 당연히 그럴 필요 없다.
