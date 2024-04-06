f3c209f0c7a31734d72663e3a195e2e12c591f3a
hyeoonjeoong <jeooooong22@gmail.com>
2024-04-06 16:01:25 +0900

    ✨ Layout 적용

--- learn-nextjs14/app/page.tsx
+++ learn-nextjs14/app/page.tsx
@@ -1,10 +1,7 @@
-import Navigation from '../components/navigation';
-
 export default function Tomato() {
   return (
     <>
       <div>
-        <Navigation />
         <h1>Main Page!</h1>
       </div>
     </>
