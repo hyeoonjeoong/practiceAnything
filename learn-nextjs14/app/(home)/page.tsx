a1885401b5a1fdeefdcceff79f6e1e306eaf883f
hyeoonjeoong <jeooooong22@gmail.com>
2024-04-16 23:20:42 +0900

    ✨ Next.js - data fetching

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -1,38 +1,72 @@
-'use client';
+//✅ -------------Next.js에서의 data fetching
+//-- useEffect, useState 없이 더 간결해졌다.
+//서버 컴포넌트를 사용하면 자동으로 fetch된 url을 캐싱시켜준다.
+//한 번만 가져오면, 어떤 데이터를 가져왔는지 기억해준다. 브라우저는 fetch를 하지 않았다.
+//개발자도구의 네트워크 탭을 확인해보자. api를 사용한 건 없다. 데이터를 가져온건 서버!
+//-- 제일 처음 fetch만 api요청하고 나머지는 요청하지 않는다. 이미 fetch된 데이터가 있고 그걸 기억한다. 그래서 첫번째 요청은 좀 느릴 수 있다.
+//즉 새로고침되는 데이터는 요청하지 않고, 기억된 메모리를 통해서 가져온다.
+//-- 그리고 로딩 state도 서버로 옮겨갔다. 클라이언트 측이였다면 화면 상 로딩중이라는 것을 볼 수 있지만,
+//지금은 서버에게 있기 때문에 클라이언트 측에서 직접적으로 볼 수 없다. setTimeout이 걸려있다면 5초가 될 때 까지 페이지 이동도 안된다.
+//서버로부터의 해당 작업이 끝날 때 까지 사용자에게 어떤 화면도 표시되지 않는 것.
 
-import { title } from 'process';
-import { useEffect, useState } from 'react';
+import { resolve } from 'path';
+
+export const metadata = {
+  title: 'Home',
+};
+
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log('fetching! 서버에서 해준다!');
+  //return fetch(URL).then(response => response.json())
+  //위 1줄이랑 아래 3줄 똑같징
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
+}
 
 //⬇⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
 //-------------기존 리액트에서의 data fetching 빙식
+
+// 'use client';
+
+// import { title } from 'process';
+// import { useEffect, useState } from 'react';
 
 //'use client'에서는 metadata 사용할 수 없다.
 // export const metadata = {
 //   title: 'Home',
 // };
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState();
-  const getMovies = async () => {
-    const response = await fetch(
-      'https://nomad-movies.nomadcoders.workers.dev/movies'
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <>
-      <div>
-        <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>
-      </div>
-    </>
-  );
-}
+// export default function Page() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [movies, setMovies] = useState();
+//   const getMovies = async () => {
+//     const response = await fetch(
+//       'https://nomad-movies.nomadcoders.workers.dev/movies'
+//     );
+//     const json = await response.json();
+//     setMovies(json);
+//     setIsLoading(false);
+//   };
+//   useEffect(() => {
+//     getMovies();
+//   }, []);
+//   return (
+//     <>
+//       <div>
+//         <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>
+//       </div>
+//     </>
+//   );
+// }
 //✅ 보통 리액트에서의 data fetching 방법이다.
 //이는 결국 사용자에게 로딩 상태를 보여줘야 한다. 그리고 항상 client 측에서 일어난다. 브라우저가 API요청을 보내게 되는 것.
 //결국 여기에는 비밀스러운 정보를 담아서 보낼 수 없다. 위험하다!
