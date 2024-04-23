22233ad83e23b619ad7d33917140c228c0adba81
hyeoonjeoong <jeooooong22@gmail.com>
2024-04-23 23:10:50 +0900

    ✨ filling mode

--- MemeMaker-project-js/app.js
+++ MemeMaker-project-js/app.js
@@ -1,3 +1,5 @@
+const modeBtn = document.getElementById('mode-btn');
+
 const colorOptions = Array.from(
   document.getElementsByClassName('color-option')
 );
@@ -10,6 +12,7 @@ canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -49,10 +52,27 @@ function onColorClick(event) {
   ctx.fillStyle = colorValue;
   color.value = colorValue;
 }
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = 'Fill';
+  } else {
+    isFilling = true;
+    modeBtn.innerText = 'Draw';
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
+canvas.addEventListener('click', onCanvasClick);
 
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener('change', onColorChange);
@@ -60,6 +80,9 @@ color.addEventListener('change', onColorChange);
 //console.log(colorOptions);
 colorOptions.forEach((color) => color.addEventListener('click', onColorClick));
 
+modeBtn.addEventListener('click', onModeClick);
+
+//-----------------------------------------------------------------
 //--------------사각형 그려보기
 // ctx.fillRect(50, 50, 100, 200); -> 얘는 단축함수
 //50, 50 이동하고 가로 100 세로 200인 사각형.
