92c203da57a5a443b42b40406f68a6232118053d
hyeoonjeoong <jeooooong22@gmail.com>
2024-05-16 16:18:46 +0900

    ✨ fetch data

--- nextjs-Prac/app/bottom/page.tsx
+++ nextjs-Prac/app/bottom/page.tsx
@@ -1,5 +1,41 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+
+const fetchData = async () => {
+  const res = await fetch('https://dummyjson.com/carts/1');
+  const data = await res.json();
+  return data.products;
+};
 
 export default function Bottom() {
-  return <div>아랫도리!</div>;
+  const [bottomData, setBottomData] = useState(null);
+
+  useEffect(() => {
+    const fetchBottom = async () => {
+      const products = await fetchData();
+      console.log(products);
+      setBottomData(products);
+    };
+    fetchBottom();
+  }, []);
+
+  return (
+    <>
+      <div>아랫도리!</div>
+      <div>
+        {bottomData &&
+          bottomData.map((item) => (
+            <>
+              <div>
+                <img src={item.thumbnail} alt={item.title} width="150px" />
+              </div>
+              <div>상품명: {item.title}</div>
+              <div>총 구매 수: {item.total}</div>
+              <div>가격: {item.price}</div>
+              <div>재고: {item.quantity}</div>
+            </>
+          ))}
+      </div>
+    </>
+  );
 }
--- nextjs-Prac/app/top/page.tsx
+++ nextjs-Prac/app/top/page.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+
+const fetchData = async () => {
+  const res = await fetch('https://dummyjson.com/carts/2');
+  const data = await res.json();
+  return data.products;
+};
 
 export default function Top() {
-  return <div>윗도리!</div>;
+  const [topItem, setTopItem] = useState(null);
+
+  useEffect(() => {
+    const fetchTop = async () => {
+      const products = await fetchData();
+      setTopItem(products);
+    };
+    fetchTop();
+  }, [topItem]);
+  return (
+    <>
+      <div>윗도리!</div>
+      <div>
+        {topItem &&
+          topItem.map((item) => (
+            <>
+              <div>
+                <img src={item.thumbnail} alt={item.title} width="150px" />
+              </div>
+              <div>상품명: {item.title}</div>
+              <div>총 구매 수: {item.total}</div>
+              <div>가격: {item.price}</div>
+              <div>재고: {item.quantity}</div>
+            </>
+          ))}
+      </div>
+    </>
+  );
 }
